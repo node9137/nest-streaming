40b4d3b5f84b2fe51d925ec1c709cb2a93619aa5
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-09-13 22:26:29 +0900

    [Comment] Swagger 연동 위한 JSDoc 추가
    Nestia 가 인식할 수 있게 Controller method 마다
    각각 method 에 대한 정보를 추가

--- src/controllers/soundtrack.controller.ts
+++ src/controllers/soundtrack.controller.ts
@@ -16,16 +16,37 @@ import { Try } from "src/types/response.type";
 export class SoundtrackController{
     constructor(private readonly soundtrackService : SoundtrackService){
     }
-    
+    /**
+     * 
+     * 게시글 생성 기능
+     * 
+     * Body 를 통해 받은 createSoundtrackRequestDto (albumName,category,name,singer,maxFormat)을 통해 Soundtrack 을 만든다.
+     * 
+     * @tag soundtrack
+     * @security bearer
+     * @param email 
+     * @param createSoundtrackRequestDto 
+     * @returns 
+     */
     @TypedRoute.Post()
     @UseGuards(JwtAuthGuard)
     async createSoundtrack(@Email()email : string,@TypedBody()createSoundtrackRequestDto : CreateSoundtrackRequestDto) : Promise<Try<null>>{
         await this.soundtrackService.create(email,createSoundtrackRequestDto);
         return {
             status:true,
-            message : "회원가입에 성공했습니다."
+            message : "음원을 생성했습니다."
         }
     }
+    /**
+     * 
+     * 음원 리스트 조회 기능
+     * 
+     * Query 를 통해 받은 GetSoundtrackQuery (skip,orderBy,take) 를 통해 Soundtrack (id,name,category) [] 을 찾아 반환한다.
+     * 
+     * @tag soundtrack
+     * @param getSoundtrackQuery 
+     * @returns 
+     */
     @TypedRoute.Get()
     async getSoundtracks(@TypedQuery()getSoundtrackQuery : GetSoundtrackQuery):Promise<Try<GetSoundtracksResponseDto[]>>{
         const data = await this.soundtrackService.getMany(getSoundtrackQuery);
@@ -34,6 +55,16 @@ export class SoundtrackController{
             data
         }
     }
+    /**
+     * 
+     * 음원 조회 기능
+     * 
+     * Param 을 통해 받은 trackId 를 통해 Soundtrack ("id"|"category"|"creatorId"|"singer"|"maxFormat"|"albumName"|"name") 을 찾아 반환한다.
+     * 
+     * @tag soundtrack
+     * @param trackId 
+     * @returns 
+     */
     @TypedRoute.Get("/:trackId")
     async getgetSoundtrack(@TypedParam("trackId")trackId : number):Promise<Try<GetSoundtrackResponseDto>>{
         const data = await this.soundtrackService.getOne(trackId);
@@ -42,6 +73,19 @@ export class SoundtrackController{
             data
         }
     }
+    /**
+     * 
+     * 음원 수정 기능
+     * 
+     * email 을 통해 작성자와 일치한지 확인 후 , updateSoundtrackRequestDto ("albumName"|"category"|"name"|"singer"|"maxFormat") 를 통해 수정한다.
+     * 
+     * @tag soundtrack
+     * @security bearer
+     * @param email 
+     * @param trackId 
+     * @param updateSoundtrackRequestDto 
+     * @returns 
+     */
     @TypedRoute.Patch("/:trackId")
     @UseGuards(JwtAuthGuard)
     async updateSoundtrack(@Email()email:string,@TypedParam("trackId")trackId : number,@TypedBody()updateSoundtrackRequestDto : UpdateSoundtrackRequestDto) : Promise<Try<null>>{
@@ -51,6 +95,17 @@ export class SoundtrackController{
             message:"음원을 수정했습니다."
         }
     }
+    /**
+     * 
+     * 음원 삭제 기능
+     * 
+     * email 을 통해 작성자와 일치한지 확인 후 , 음원을 삭제한다.
+     * @tag soundtrack
+     * @security bearer
+     * @param email 
+     * @param trackId 
+     * @returns 
+     */
     @TypedRoute.Delete("/:trackId")
     @UseGuards(JwtAuthGuard)
     async deleteSoundtrack(@Email()email:string,@TypedParam("trackId")trackId : number):Promise<Try<null>>{
