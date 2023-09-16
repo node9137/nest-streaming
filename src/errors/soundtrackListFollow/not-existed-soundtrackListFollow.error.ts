b7ad6299e385bf262be139ffeb5fd5b3ecede77c
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-09-16 13:09:47 +0900

    [Edit] code 부분 수정
    미처 수정하지 못한 code 부분 수정

--- src/errors/soundtrackListFollow/not-existed-soundtrackListFollow.error.ts
+++ src/errors/soundtrackListFollow/not-existed-soundtrackListFollow.error.ts
@@ -2,7 +2,7 @@ import { ExceptionBase } from "../exception.base";
 
 export class NotExistedSoundtrackListFollow extends ExceptionBase{
     static readonly message = "회원이 팔로우한 음원이 아닙니다."
-    public readonly code = 'USER.NOT_EXIST'
+    public readonly code = 'USER.NOT_EXISTED_SOUNDTRACK_LIST_FOLLOW'
     static readonly businessCode = 1032
     constructor(cause?:Error,metadata?:unknown){
         super(NotExistedSoundtrackListFollow.message,NotExistedSoundtrackListFollow.businessCode,cause)
